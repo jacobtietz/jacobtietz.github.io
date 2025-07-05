2d8516ad5abc2d3e8c44d581a695e385eece8151
Jacob Tietz <jaetietz@gmail.com>
2025-07-05 17:22:51 -0500

    Added background and basics

---
+++ particles-config.js
@@ -0,0 +1,43 @@
+particlesJS("particles-js", {
+  "particles": {
+    "number": {
+      "value": 75,
+      "density": {
+        "enable": true,
+        "value_area": 800
+      }
+    },
+    "color": {
+      "value": "#ffffff"
+    },
+    "shape": {
+      "type": "circle"
+    },
+    "opacity": {
+      "value": 0.5
+    },
+    "size": {
+      "value": 3
+    },
+    "line_linked": {
+      "enable": true,
+      "distance": 150,
+      "color": "#ffffff",
+      "opacity": 0.4,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "speed": 2
+    }
+  },
+  "interactivity": {
+    "events": {
+      "onhover": {
+        "enable": true,
+        "mode": "grab"
+      }
+    }
+  },
+  "retina_detect": true
+});
